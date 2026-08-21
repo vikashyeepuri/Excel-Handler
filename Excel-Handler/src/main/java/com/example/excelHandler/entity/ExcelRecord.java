e3de1f904c463d4e3638769e266371c9209b733c
Arshadkhan0007 <contactkhan0007@gmail.com>
2026-08-21 14:35:45 +0530

    changed table and column names as per requirement

--- Excel-Handler/src/main/java/com/example/excelHandler/entity/ExcelRecord.java
+++ Excel-Handler/src/main/java/com/example/excelHandler/entity/ExcelRecord.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 
 @Entity
-@Table(name = "excel_records")
+@Table(name = "EXCEL_RECORDS")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,110 +17,115 @@ import java.math.BigDecimal;
 public class ExcelRecord {
 
     @Id
-    @SequenceGenerator(name = "excelRecordSeq", sequenceName = "excel_record_seq")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "excelRecordSeq")
+    @SequenceGenerator(
+            name = "excelRecordSeq",
+            sequenceName = "EXCEL_RECORD_SEQ"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "excelRecordSeq"
+    )
     private Long id;
 
-    @Column(name = "batch_transaction_id")
+    @Column(name = "BATCH_TRANSACTION_ID")
     private String batchTransactionId;
 
-    @Column(name = "product_id")
+    @Column(name = "PRODUCT_ID")
     private String productId;
 
-    @Column(name = "debit_ref_no", unique = true)
+    @Column(name = "DEBIT_REF_NO", unique = true)
     private String debitRefNo;
 
-    @Column(name = "debit_account_no")
+    @Column(name = "DEBIT_ACCOUNT_NO")
     private String debitAccountNo;
 
-    @Column(name = "transfer_branch")
+    @Column(name = "TRANSFER_BRANCH")
     private String transferBranch;
 
-    @Column(name = "debit_currency")
+    @Column(name = "DEBIT_CURRENCY")
     private String debitCurrency;
 
-    @Column(name = "debit_narration1")
+    @Column(name = "DEBIT_NARRATION1")
     private String debitNarration1;
 
-    @Column(name = "internal_acc_flag")
+    @Column(name = "INTERNAL_ACC_FLAG")
     private String internalAccFlag;
 
-    @Column(name = "order_cust1")
+    @Column(name = "ORDER_CUST1")
     private String orderCust1;
 
-    @Column(name = "order_cust2")
+    @Column(name = "ORDER_CUST2")
     private String orderCust2;
 
-    @Column(name = "order_cust3")
+    @Column(name = "ORDER_CUST3")
     private String orderCust3;
 
-    @Column(name = "order_cust4")
+    @Column(name = "ORDER_CUST4")
     private String orderCust4;
 
-    @Column(name = "credit_account_no")
+    @Column(name = "CREDIT_ACCOUNT_NO")
     private String creditAccountNo;
 
-    @Column(name = "transaction_code")
+    @Column(name = "TRANSACTION_CODE")
     private String transactionCode;
 
-    @Column(name = "amount")
+    @Column(name = "AMOUNT")
     private BigDecimal amount;
 
-    @Column(name = "credit_currency")
+    @Column(name = "CREDIT_CURRENCY")
     private String creditCurrency;
 
-    @Column(name = "credit_narration1")
+    @Column(name = "CREDIT_NARRATION1")
     private String creditNarration1;
 
-    @Column(name = "charge_bearer")
+    @Column(name = "CHARGE_BEARER")
     private String chargeBearer;
 
-    @Column(name = "payment_details")
+    @Column(name = "PAYMENT_DETAILS")
     private String paymentDetails;
 
-    @Column(name = "ben_name")
+    @Column(name = "BEN_NAME")
     private String benName;
 
-    @Column(name = "ben_addr1")
+    @Column(name = "BEN_ADDR1")
     private String benAddr1;
 
-    @Column(name = "ben_addr2")
+    @Column(name = "BEN_ADDR2")
     private String benAddr2;
 
-    @Column(name = "ben_addr3")
+    @Column(name = "BEN_ADDR3")
     private String benAddr3;
 
-    @Column(name = "aw_inst_bic_code")
+    @Column(name = "AW_INST_BIC_CODE")
     private String awInstBicCode;
 
-    @Column(name = "aw_inst_name")
+    @Column(name = "AW_INST_NAME")
     private String awInstName;
 
-    @Column(name = "aw_inst_addr1")
+    @Column(name = "AW_INST_ADDR1")
     private String awInstAddr1;
 
-    @Column(name = "aw_inst_addr2")
+    @Column(name = "AW_INST_ADDR2")
     private String awInstAddr2;
 
-    @Column(name = "aw_inst_addr3")
+    @Column(name = "AW_INST_ADDR3")
     private String awInstAddr3;
 
-    @Column(name = "trans_type_code")
+    @Column(name = "TRANS_TYPE_CODE")
     private String transTypeCode;
 
-    @Column(name = "sender_to_reciever_info")
+    @Column(name = "SENDER_TO_RECIEVER_INFO")
     private String senderToRecieverInfo;
 
-    @Column(name = "empty")
+    @Column(name = "EMPTY")
     private String empty;
 
-    @Column(name = "x_msg_id")
+    @Column(name = "X_MSG_ID")
     private String xMsgId;
 
-    @Column(name = "transaction_ref_no")
+    @Column(name = "TRANSACTION_REF_NO")
     private String transactionRefNo;
 
-    @Column(name = "status")
+    @Column(name = "STATUS")
     private String status;
-
 }
